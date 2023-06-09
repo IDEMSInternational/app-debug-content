829e6a788d0c033af904bebc6aea22311a721906
Johnny McQuade <jfmcquade@hotmail.com>
2023-06-09 15:09:54 +0100

    content: 1.1.9

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 import { generateDeploymentConfig } from "scripts";
 const config = generateDeploymentConfig("debug");
+import { SKINS } from "./skins";
 
 config.google_drive = {
   sheets_folder_id: "1YcG0GDJME1Ds1UzwXcEmo97Ckb5tnUWc",
@@ -8,7 +9,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/app-debug-content.git",
-  content_tag_latest: "1.1.8",
+  content_tag_latest: "1.1.9",
 };
 
 config.app_data.output_path = "./app_data";
@@ -16,5 +17,7 @@ config.app_data.output_path = "./app_data";
 // Override any app constants here
 config.app_config.APP_HEADER_DEFAULTS.title = "Debug App";
 config.app_config.APP_SIDEMENU_DEFAULTS.title = "Debug App";
+config.app_config.APP_SKINS.defaultSkinName = SKINS.defaultSkin.name
+config.app_config.APP_SKINS.available = [SKINS.defaultSkin, SKINS.debug]
 
 export default config;
