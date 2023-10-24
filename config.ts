764571166c73a94b403b706f2dc99bb8775931e1
EsmeeIDEMS <esmee@idems.international>
2023-10-24 17:05:31 +0100

    content: 1.1.35

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/app-debug-content.git",
-  content_tag_latest: "1.1.34",
+  content_tag_latest: "1.1.35",
 };
 
 config.app_data.output_path = "./app_data";
