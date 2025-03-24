1a2a990a5507c47324739014c4865b3a7f9e7523
EsmeeIDEMS <esmee@idems.international>
2025-03-24 16:10:24 +0000

    content: 1.4.17

--- config.ts
+++ config.ts
@@ -14,7 +14,7 @@ config.web.favicon_asset = "images/icons/favicon.svg";
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/app-debug-content.git",
-  content_tag_latest: "1.4.16",
+  content_tag_latest: "1.4.17",
 };
 
 config.app_config.ASSET_PACKS = {
