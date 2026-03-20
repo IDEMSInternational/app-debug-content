d6e031772eb8cbd5daac5df62e481fb53a7d3700
Johnny McQuade <jfmcquade@hotmail.com>
2026-03-20 14:15:51 +0000

    chore: version bump 1.10.4

--- config.ts
+++ config.ts
@@ -16,7 +16,7 @@ config.web.favicon_asset = "images/icons/favicon.svg";
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/app-debug-content.git",
-  content_tag_latest: "1.10.3",
+  content_tag_latest: "1.10.4",
 };
 
 // set supabase config if decrypted values available
