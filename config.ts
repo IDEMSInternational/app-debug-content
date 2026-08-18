28ef545e82e324d0ac44ba0372492d12bfe9d072
Johnny McQuade <jfmcquade@hotmail.com>
2026-08-18 13:08:07 +0100

    chore: bump version number to 1.11.0

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ const config = generateDeploymentConfig("debug");
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/app-debug-content.git",
-  content_tag_latest: "1.10.14",
+  content_tag_latest: "1.11.0",
 };
 
 config.api.db_name = "debug"
